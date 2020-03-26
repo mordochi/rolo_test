1ddf77bcd7b8a16fb9eef806f309c7c05825e6b4
Lynn Chi <lynn@deepcodify.com>
2020-03-27 00:17:30 +0800

    add <SearchBar />

---
+++ src/Component/SearchBar/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from '../Button';
+import Add from '../icons/Add';
+import Input from '../Input';
+import { Container } from './styles';
+
+const Panel = () => {
+  return (
+    <Container>
+      <Input width="calc(100% - 48px)" />
+      <Button>
+        <Add />
+      </Button>
+    </Container>
+  );
+};
+
+export default Panel;
---
+++ src/Component/SearchBar/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 74px;
+  padding: 26px 16px 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  backdrop-filter: blur(32.6194px);
+  display: flex;
+  justify-content: space-between;
+`;
