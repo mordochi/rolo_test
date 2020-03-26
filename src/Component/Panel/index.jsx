3fd4913620ed8caf12773e99673e96d22919e4a4
Lynn Chi <lynn@deepcodify.com>
2020-03-27 00:18:00 +0800

    add <Panel />

---
+++ src/Component/Panel/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './styles';
+import SearchBar from '../SearchBar';
+
+const SideBar = () => {
+  return (
+    <Container>
+      <SearchBar />
+    </Container>
+  );
+};
+
+export default SideBar;
---
+++ src/Component/Panel/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  flex: 4;
+  color: #ffffff;
+  border-right: solid 1px rgba(241, 241, 241, 0.2);
+`;
