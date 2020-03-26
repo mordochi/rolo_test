c1ebf963d51177ad6c9d0a8694ccee0824ed976a
Lynn Chi <lynn@deepcodify.com>
2020-03-27 00:17:45 +0800

    add <ChatRoom />

---
+++ src/Component/ChatRoom/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './styles';
+
+const ChatRoom = () => {
+  return <Container>ChatRoom</Container>;
+};
+
+export default ChatRoom;
---
+++ src/Component/ChatRoom/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  flex: 7;
+  color: #ffffff;
+`;
