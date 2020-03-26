8d4612ea78a279de4cf22f8014e5be734325e049
Lynn Chi <lynn@deepcodify.com>
2020-03-27 00:16:47 +0800

    add <Button />

---
+++ src/Component/Button/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  width: 40px;
+  height: 40px;
+  border: none;
+  border-radius: 50%;
+  background-color: rgba(241, 241, 241, 0.08);
+`;
