1ea55da8f290798ef982f9e13cadef6bd7fddbc3
Lynn Chi <lynn@deepcodify.com>
2020-03-28 16:58:24 +0800

    update <ChatRoom />

--- src/Component/ChatRoom/index.jsx
+++ src/Component/ChatRoom/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from './styles';
 
 const ChatRoom = () => {
-  return <Container>ChatRoom</Container>;
+  return <Container>Chat Room</Container>;
 };
 
 export default ChatRoom;
--- src/Component/ChatRoom/styles.js
+++ src/Component/ChatRoom/styles.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  flex: 7;
+  flex: 7 0 0;
   color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  font-weight: 500;
 `;
